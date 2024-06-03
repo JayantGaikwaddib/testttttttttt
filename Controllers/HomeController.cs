005a0eb7c12e3745918b1bcef41513a8f2439e70
JayantGaikwaddib <jayant@digitalinfobytes.com>
2024-06-03 10:56:47 +0530

    second for rebase

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,11 @@ namespace testttttttt.Controllers
         {
             return View();
         }
+
+        public IActionResult Index1221()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
